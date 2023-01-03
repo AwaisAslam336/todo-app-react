9184ca67217eae1177e8da9283c612f24f2a5077
Awais Aslam <awaisaslam336@gmail.com>
2023-01-03 22:55:34 +0500

    add tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,37 @@
 import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [newItem, setNewItem] = useState("");
+  const [getItems, setItems] = useState([]);
+
+  function show(item,index){
+    return (<li key={index}>{item}</li>);
+  }
+  function submited(){
+    setItems((preValue)=>{
+      return [...preValue,newItem]
+    });
+  }
+  function itemValue(event){
+    const value = event.target.value;
+    setNewItem(value);
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
-        <button>
+        <input type="text" onChange={itemValue} value={newItem} />
+        <button onClick={submited}>
           <span>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item</li>
+          {getItems.map(show)}
         </ul>
       </div>
     </div>
